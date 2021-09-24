61afda98cf15951011a2fcffea0c8dd692c4cc7b
xLunaBoyx <sonic_x@naver.com>
2021-09-24 10:45:19 +0900

    sayFoo 기능 추가!

--- 01_hello_git/src/com/kh/git/HelloWorld.java
+++ 01_hello_git/src/com/kh/git/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.kh.git;
 
+import com.kh.foo.Foo;
+
 public class HelloWorld {
 
 	public static void main(String[] args_ ) {
 		System.out.println("Hello world~");
 		System.out.println("Hello git~");
+		
+		new Foo().sayFoo();
 	}
 }
