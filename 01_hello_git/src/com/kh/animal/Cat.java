a36c1ee2e021e781ceb69ba10b0bfc05802c65a4
xLunaBoyx <sonic_x@naver.com>
2021-09-24 11:42:47 +0900

    Dog Cat 클래스 생성

---
+++ 01_hello_git/src/com/kh/animal/Cat.java
@@ -0,0 +1,7 @@
+package com.kh.animal;
+
+public class Cat {
+	public void say() {
+		System.out.println("냐옹");
+	}
+}
---
+++ 01_hello_git/src/com/kh/animal/Dog.java
@@ -0,0 +1,7 @@
+package com.kh.animal;
+
+public class Dog {
+	public void say() {
+		System.out.println("멍멍");
+	}
+}
